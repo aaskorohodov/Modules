65c8264ae557a5df1950772f75bd713618909970
Arkady <aaskorohodov@gmail.com>
2021-08-24 17:30:49 +0500

    Обновлена структура каталога Git

--- Git/Learning Gir/Some Func.py
+++ Git/Learning Gir/Some Func.py
@@ -2,5 +2,20 @@ def up(text):
     return text.upper()
 
 
+def plus(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+
+
+def multupl(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
     print(up('hello!'))
+    print(plus(2, 2))
+    print(minus(3, 2))
+    print(multupl(2, 5))
