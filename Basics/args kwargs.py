fa071d48c5f24c7d428e272e2640e8ccd2fabd34
Arkady <aaskorohodov@gmail.com>
2021-09-19 15:25:33 +0500

    +

---
+++ Basics/args kwargs.py
@@ -0,0 +1,14 @@
+# стандартный метод передачи в функцию неименованных и именованных аргументов:
+def my_func(*args, **kwargs):
+    pass
+
+
+# **kwargs требует именования аргументов внутри вызова функции. Аргументы распаковываются в словарь
+def my_kwargs_func(**kwargs):
+    print(kwargs)
+
+    for k, v in kwargs.items():
+        print(k, v)
+
+
+my_kwargs_func(a = 1, b = 2, c = 3)
